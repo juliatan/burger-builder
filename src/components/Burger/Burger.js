58f036b8dda2b4cbf4514f67d0d6d648d23bcbe1
Julia Tan <juliajtan@gmail.com>
2020-09-05 16:37:43 +0100

    Handle situation where no ingredients have been added

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,15 +3,23 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 import classes from './Burger.module.css';
 
 const burger = (props) => {
-  const transformedIngredients = Object.keys(props.ingredients).map(
-    (ingredientKey) => {
+  // want to transform the ingredients state to array of ingredients
+  let transformedIngredients = Object.keys(props.ingredients)
+    .map((ingredientKey) => {
       return [...Array(props.ingredients[ingredientKey])].map((_, index) => {
         return (
           <BurgerIngredient type={ingredientKey} key={ingredientKey + index} />
         );
       });
-    },
-  );
+    })
+    // collapse arrays to count all ingredients to check if it's an empty burger
+    .reduce((arr, element) => {
+      return arr.concat(element);
+    }, []);
+
+  if (transformedIngredients.length === 0) {
+    transformedIngredients = <p>Start adding your ingredients!</p>;
+  }
 
   return (
     <div className={classes.Burger}>
