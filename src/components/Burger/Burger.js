83562dc136d7c281e96e98f5b92a4e2656f3128c
Julia Tan <juliajtan@gmail.com>
2020-09-05 16:24:18 +0100

    Passing dynamic ingredients into Burger

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -2,12 +2,21 @@ import React from 'react';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 import classes from './Burger.module.css';
 
-const burger = () => {
+const burger = (props) => {
+  const transformedIngredients = Object.keys(props.ingredients).map(
+    (ingredientKey) => {
+      return [...Array(props.ingredients[ingredientKey])].map((_, index) => {
+        return (
+          <BurgerIngredient type={ingredientKey} key={ingredientKey + index} />
+        );
+      });
+    },
+  );
+
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top" />
-      <BurgerIngredient type="cheese" />
-      <BurgerIngredient type="meat" />
+      {transformedIngredients}
       <BurgerIngredient type="bread-bottom" />
     </div>
   );
