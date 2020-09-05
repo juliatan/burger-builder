bbcd5400e4b2b2225bd0bbf5f78039f7f7f9c9e1
Julia Tan <juliajtan@gmail.com>
2020-09-05 13:24:42 +0100

    Added BurgerBuilder container and CSS module

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import Aux from '../../hoc/Aux/Aux';
+import classes from './Layout.module.css';
 
 const layout = (props) => (
   <Aux>
     <div>Toolbar, Sidebar, Backdrop</div>
-    <main>
-      {props.children}
-    </main>
+    <main className={classes.Content}>{props.children}</main>
   </Aux>
 );
 
---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import Aux from '../../hoc/Aux/Aux';
+
+class BurgerBuilder extends Component {
+  render() {
+    return (
+      <Aux>
+        <div> Burger</div>
+        <div>Build controls</div>
+      </Aux>
+    );
+  }
+}
+
+export default BurgerBuilder;
