bbcf43939088eb91d382d8679b45eca1d4d46abd
Julia Tan <juliajtan@gmail.com>
2020-09-26 14:36:08 +0100

    Fix bug to do with dropdown

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -83,7 +83,8 @@ class ContactData extends Component {
             { value: 'cheapest', displayValue: 'Cheapest' },
           ],
         },
-        value: '',
+        value: 'fastest',
+        validation: {},
         valid: true, // needed to ensure formIsValid properly updated below
       },
     },
