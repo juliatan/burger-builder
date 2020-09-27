8d3a7d8deaf491070744584c4435b414049c2c17
Julia Tan <juliajtan@gmail.com>
2020-09-27 17:42:57 +0100

    Install redux-thunk and configure in index.js to prepare us for using async functions in action creators

--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import './index.css';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+
+import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import burgerBuilderReducer from './store/reducers/burgerBuilder';
 
+// https://github.com/zalmoxisus/redux-devtools-extension
+// compose allows us to use different enhancers, one of which is Middleware, and another is the redux-devtools
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
   burgerBuilderReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  composeEnhancers(applyMiddleware(thunk)),
 );
 
 ReactDOM.render(
