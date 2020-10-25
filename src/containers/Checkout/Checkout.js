daac5fea80ddab0042d0aeccfea5905b6af13d61
Julia Tan <juliajtan@gmail.com>
2020-10-25 18:51:55 +0000

    If on checkout route but no ingredients, redirect to start route

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
 import ContactData from './ContactData/ContactData';
@@ -25,28 +25,32 @@ class Checkout extends Component {
   };
 
   render() {
-    return (
-      <div>
-        <CheckoutSummary
-          ingredients={this.props.ings}
-          checkoutCancelled={this.checkoutCancelledHandler}
-          checkoutContinued={this.checkoutContinuedHandler}
-        />
-        <Route
-          // eslint-disable-next-line prefer-template
-          path={this.props.match.path + '/contact-data'}
-          component={ContactData}
-          // replaced with render to pass in our own props, and the router props
-          // render={(props) => (
-          //   <ContactData
-          //     ingredients={this.props.ings}
-          //     totalPrice={this.props.price}
-          //     {...props}
-          //   />
-          // )}
-        />
-      </div>
-    );
+    let summary = <Redirect to="/" />;
+    if (this.props.ings) {
+      summary = (
+        <div>
+          <CheckoutSummary
+            ingredients={this.props.ings}
+            checkoutCancelled={this.checkoutCancelledHandler}
+            checkoutContinued={this.checkoutContinuedHandler}
+          />
+          <Route
+            // eslint-disable-next-line prefer-template
+            path={this.props.match.path + '/contact-data'}
+            component={ContactData}
+            // replaced with render to pass in our own props, and the router props
+            // render={(props) => (
+            //   <ContactData
+            //     ingredients={this.props.ings}
+            //     totalPrice={this.props.price}
+            //     {...props}
+            //   />
+            // )}
+          />
+        </div>
+      );
+    }
+    return summary;
   }
 }
 
