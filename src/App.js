75c88f56fbe49bc973451a37382668ec3ec1cc62
Julia Tan <juliajtan@gmail.com>
2020-10-26 16:17:36 +0000

    Create Logout component that calls logout action immediately on click

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
 import Orders from './containers/Orders/Orders';
 import Auth from './containers/Auth/Auth';
+import Logout from './containers/Auth/Logout/Logout';
 
 // eslint-disable-next-line react/prefer-stateless-function
 class App extends Component {
@@ -14,6 +15,7 @@ class App extends Component {
         <Layout>
           <Switch>
             <Route path="/auth" component={Auth} />
+            <Route path="/logout" component={Logout} />
             <Route path="/checkout" component={Checkout} />
             <Route path="/orders" component={Orders} />
             <Route path="/" exact component={BurgerBuilder} />
