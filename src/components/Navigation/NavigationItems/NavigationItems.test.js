d20e5819624082e765ca231872145a9dd3ab8a91
Julia Tan <juliajtan@gmail.com>
2020-10-28 10:33:51 +0000

    Add another test to NavigationItems

--- src/components/Navigation/NavigationItems/NavigationItems.test.js
+++ src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -27,4 +27,11 @@ describe('NavigationItems', () => {
     wrapper.setProps({ isAuthenticated: true });
     expect(wrapper.find(NavigationItem)).toHaveLength(3);
   });
+
+  it('should show logout link if authenticated', () => {
+    wrapper.setProps({ isAuthenticated: true });
+    expect(
+      wrapper.contains(<NavigationItem link="/logout">Logout</NavigationItem>),
+    ).toEqual(true);
+  });
 });
